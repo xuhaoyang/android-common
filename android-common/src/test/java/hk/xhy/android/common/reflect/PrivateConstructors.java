a2073e6517d66d56256b744430d727b6514e431e
XHY <497633959@qq.com>
2018-01-11 10:31:08 +0800

    添加反射工具 更新正则工具

---
+++ android-common/src/test/java/hk/xhy/android/common/reflect/PrivateConstructors.java
@@ -0,0 +1,22 @@
+package hk.xhy.android.common.reflect;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2017/05/26
+ *     desc  : CacheUtils 单元测试
+ * </pre>
+ */
+public class PrivateConstructors {
+
+    public final String string;
+
+    private PrivateConstructors() {
+        this(null);
+    }
+
+    private PrivateConstructors(String string) {
+        this.string = string;
+    }
+}
